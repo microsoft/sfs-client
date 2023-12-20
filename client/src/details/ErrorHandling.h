3b72e685b492ccce37b32468d51675c45368610c
Arthur Araujo <arthuraraujo@microsoft.com>
2023-12-20 15:31:37 -0300

    Adding an internal exception type that can be thrown in constructors and caught by the return macro

--- client/src/details/ErrorHandling.h
+++ client/src/details/ErrorHandling.h
@@ -3,10 +3,18 @@
 
 #pragma once
 
+#include "Result.h"
+
+#include <exception>
+
 #define SFS_CATCH_RETURN()                                                                                             \
     catch (const std::bad_alloc&)                                                                                      \
     {                                                                                                                  \
         return Result::E_OutOfMemory;                                                                                  \
+    }                                                                                                                  \
+    catch (const SFS::details::SFSException& e)                                                                        \
+    {                                                                                                                  \
+        return e.GetResult();                                                                                          \
     }                                                                                                                  \
     catch (const std::exception&)                                                                                      \
     {                                                                                                                  \
@@ -26,3 +34,28 @@
             return __result;                                                                                           \
         }                                                                                                              \
     } while ((void)0, 0)
+
+namespace SFS::details
+{
+class SFSException : public std::exception
+{
+  public:
+    SFSException() = default;
+    SFSException(SFS::Result result) : m_result(std::move(result))
+    {
+    }
+
+    const SFS::Result& GetResult() const noexcept
+    {
+        return m_result;
+    }
+
+    const char* what() const noexcept override
+    {
+        return m_result.GetMessage().c_str();
+    }
+
+  private:
+    SFS::Result m_result;
+};
+} // namespace SFS::details
