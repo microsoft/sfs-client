54fbaf77ad4d69cd7f96b8f12e93a9746276fcb6
Arthur Araujo <arthuraraujo@microsoft.com>
2024-01-02 12:44:57 -0300

    Checking for expected runtime curl features

--- client/src/details/connection/ConnectionManager.cpp
+++ client/src/details/connection/ConnectionManager.cpp
@@ -8,7 +8,27 @@
 
 #include <curl/curl.h>
 
+using namespace SFS;
 using namespace SFS::details;
+
+namespace
+{
+// Curl recommends checking for expected features in runtime
+void CheckCurlFeatures(const ReportingHandler& handler)
+{
+    curl_version_info_data* ver = curl_version_info(CURLVERSION_NOW);
+    THROW_CODE_IF_LOG(E_HttpUnexpected, !ver, handler);
+
+    THROW_CODE_IF_LOG(E_HttpUnexpected, !(ver->features & CURL_VERSION_SSL), handler, "Curl was not built with SSL");
+    THROW_CODE_IF_LOG(E_HttpUnexpected, !(ver->features & CURL_VERSION_THREADSAFE), handler, "Curl is not thread safe");
+
+    // For thread safety we need the DNS resolutions to be asynchronous (which happens because of c-ares)
+    THROW_CODE_IF_LOG(E_HttpUnexpected,
+                      !(ver->features & CURL_VERSION_ASYNCHDNS),
+                      handler,
+                      "Curl was not built with async DNS resolutions");
+}
+} // namespace
 
 ConnectionManager::ConnectionManager(const ReportingHandler& handler) : m_handler(handler)
 {
@@ -16,11 +36,11 @@ ConnectionManager::ConnectionManager(const ReportingHandler& handler) : m_handle
 
 CurlConnectionManager::CurlConnectionManager(const ReportingHandler& handler) : ConnectionManager(handler)
 {
-    auto result = curl_global_init(CURL_GLOBAL_ALL);
-    if (result != CURLE_OK)
-    {
-        throw SFSException(Result(Result::E_Unexpected, "Curl failed to initialize"));
-    }
+    THROW_CODE_IF_LOG(E_HttpUnexpected,
+                      curl_global_init(CURL_GLOBAL_ALL) != CURLE_OK,
+                      m_handler,
+                      "Curl failed to initialize");
+    CheckCurlFeatures(m_handler);
 }
 
 CurlConnectionManager::~CurlConnectionManager()
