c17fa25dc38869c576ea00fe5adae08aaa75f342
Arthur Araujo <arthuraraujo@microsoft.com>
2024-02-09 11:36:11 -0300

    Adding ASCII tests for AreEqualI

---
+++ client/tests/unit/details/UtilTests.cpp
@@ -0,0 +1,25 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+#include "Util.h"
+
+#include <catch2/catch_test_macros.hpp>
+
+#define TEST(...) TEST_CASE("[UtilTests] " __VA_ARGS__)
+
+using namespace SFS::details::util;
+
+TEST("Testing AreEqualI")
+{
+    SECTION("ASCII strings")
+    {
+        REQUIRE(AreEqualI("abc", "abc"));
+        REQUIRE(AreEqualI("abc", "ABC"));
+        REQUIRE(AreEqualI("abc", "Abc"));
+        REQUIRE(AreEqualI("abc", "aBc"));
+        REQUIRE(AreEqualI("abc", "abC"));
+        REQUIRE(AreEqualI("abc", "ABC"));
+        REQUIRE(AreEqualI("abc", "AbC"));
+        REQUIRE(AreEqualI("abc", "aBC"));
+    }
+}
