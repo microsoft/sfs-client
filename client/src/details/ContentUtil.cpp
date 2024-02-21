14a7843474d3e146c15a51f79330a8c0e71037ad
Arthur Araujo <arthuraraujo@microsoft.com>
2024-02-21 16:18:57 -0300

    Adding DO Data parsing to ContentUtil

--- client/src/details/ContentUtil.cpp
+++ client/src/details/ContentUtil.cpp
@@ -93,6 +93,48 @@ std::unique_ptr<File> contentutil::FileJsonToObj(const json& file, const Reporti
     return tmp;
 }
 
+std::unique_ptr<DeliveryOptimizationData> contentutil::FileJsonToDODataObj(const nlohmann::json& file,
+                                                                           const ReportingHandler& handler)
+{
+    ThrowInvalidResponseIfFalse(file.is_object(), "File is not a JSON object", handler);
+
+    ThrowInvalidResponseIfFalse(file.contains("DeliveryOptimization"),
+                                "Missing File.DeliveryOptimization in response",
+                                handler);
+    ThrowInvalidResponseIfFalse(file["DeliveryOptimization"].is_object(),
+                                "File.DeliveryOptimization is not a JSON object",
+                                handler);
+
+    const auto& deliveryOptimization = file["DeliveryOptimization"];
+
+    ThrowInvalidResponseIfFalse(deliveryOptimization.contains("CatalogId"),
+                                "Missing File.DeliveryOptimization.CatalogId in response",
+                                handler);
+    ThrowInvalidResponseIfFalse(deliveryOptimization["CatalogId"].is_string(),
+                                "File.DeliveryOptimization.CatalogId is not a string",
+                                handler);
+    std::string catalogId = deliveryOptimization["CatalogId"];
+
+    ThrowInvalidResponseIfFalse(deliveryOptimization.contains("Properties"),
+                                "Missing File.DeliveryOptimization.Properties in response",
+                                handler);
+    ThrowInvalidResponseIfFalse(deliveryOptimization["Properties"].is_object(),
+                                "File.DeliveryOptimization.Properties is not a JSON object",
+                                handler);
+
+    std::unordered_map<std::string, std::string> properties;
+    for (const auto& [key, value] : deliveryOptimization["Properties"].items())
+    {
+        // The value can be any JSON object. We will only dump it as-is in JSON form.
+        properties[key] = value.dump();
+    }
+
+    std::unique_ptr<DeliveryOptimizationData> tmp;
+    THROW_IF_FAILED_LOG(DeliveryOptimizationData::Make(std::move(catalogId), std::move(properties), tmp), handler);
+
+    return tmp;
+}
+
 bool contentutil::operator==(const ContentId& lhs, const ContentId& rhs)
 {
     // String characters can be UTF-8 encoded, so we need to compare them in a case-sensitive manner.
--- client/src/details/ContentUtil.h
+++ client/src/details/ContentUtil.h
@@ -4,6 +4,7 @@
 #pragma once
 
 #include "Content.h"
+#include "DeliveryOptimizationData.h"
 
 #include <nlohmann/json_fwd.hpp>
 
@@ -21,6 +22,8 @@ namespace contentutil
 
 std::unique_ptr<ContentId> ContentIdJsonToObj(const nlohmann::json& contentId, const ReportingHandler& handler);
 std::unique_ptr<File> FileJsonToObj(const nlohmann::json& file, const ReportingHandler& handler);
+std::unique_ptr<DeliveryOptimizationData> FileJsonToDODataObj(const nlohmann::json& file,
+                                                              const ReportingHandler& handler);
 
 //
 // Comparison operators
