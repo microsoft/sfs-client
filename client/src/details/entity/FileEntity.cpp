80fea4610055d6b38a537575fa0a5d48fa6a0649
Arthur Araujo <arthuraraujo@microsoft.com>
2024-02-29 12:55:28 -0300

    Parsing AppFileEntity

--- client/src/details/entity/FileEntity.cpp
+++ client/src/details/entity/FileEntity.cpp
@@ -27,12 +27,33 @@ std::unique_ptr<FileEntity> FileEntity::FromJson(const nlohmann::json& file, con
     //   },
     //   "DeliveryOptimization": {} // ignored, not used by the client.
     // }
-
-    // TODO #50: Use a different entity once the service supports app content.
-
-    std::unique_ptr<FileEntity> tmp = std::make_unique<GenericFileEntity>();
+    //
+    // Expected extra elements for an app version entity:
+    // {
+    //   ...
+    //   "ApplicabilityDetails": {
+    //     "Architectures": [
+    //       "<arch>"
+    //     ],
+    //     "PlatformApplicabilityForPackage": [
+    //       "<app>"
+    //     ]
+    //   },
+    //   "FileMoniker": "<moniker>",
+    // }
 
     THROW_INVALID_RESPONSE_IF_NOT(file.is_object(), "File is not a JSON object", handler);
+
+    std::unique_ptr<FileEntity> tmp;
+    const bool isAppEntity = file.contains("FileMoniker");
+    if (isAppEntity)
+    {
+        tmp = std::make_unique<AppFileEntity>();
+    }
+    else
+    {
+        tmp = std::make_unique<GenericFileEntity>();
+    }
 
     THROW_INVALID_RESPONSE_IF_NOT(file.contains("FileId"), "Missing File.FileId in response", handler);
     THROW_INVALID_RESPONSE_IF_NOT(file["FileId"].is_string(), "File.FileId is not a string", handler);
@@ -55,6 +76,50 @@ std::unique_ptr<FileEntity> FileEntity::FromJson(const nlohmann::json& file, con
     {
         THROW_INVALID_RESPONSE_IF_NOT(hashValue.is_string(), "File.Hashes object value is not a string", handler);
         tmp->hashes[hashType] = hashValue;
+    }
+
+    if (isAppEntity)
+    {
+        auto appEntity = dynamic_cast<AppFileEntity*>(tmp.get());
+
+        THROW_INVALID_RESPONSE_IF_NOT(file["FileMoniker"].is_string(), "File.FileMoniker is not a string", handler);
+        appEntity->fileMoniker = file["FileMoniker"];
+
+        THROW_INVALID_RESPONSE_IF_NOT(file.contains("ApplicabilityDetails"),
+                                      "Missing File.ApplicabilityDetails in response",
+                                      handler);
+
+        const auto& details = file["ApplicabilityDetails"];
+        THROW_INVALID_RESPONSE_IF_NOT(details.is_object(), "File.ApplicabilityDetails is not an object", handler);
+
+        THROW_INVALID_RESPONSE_IF_NOT(details.contains("Architectures"),
+                                      "Missing File.ApplicabilityDetails.Architectures in response",
+                                      handler);
+        THROW_INVALID_RESPONSE_IF_NOT(details["Architectures"].is_array(),
+                                      "File.ApplicabilityDetails.Architectures is not an array",
+                                      handler);
+        for (const auto& arch : details["Architectures"])
+        {
+            THROW_INVALID_RESPONSE_IF_NOT(arch.is_string(),
+                                          "File.ApplicabilityDetails.Architectures array value is not a string",
+                                          handler);
+        }
+        appEntity->applicabilityDetails.architectures = details["Architectures"];
+
+        THROW_INVALID_RESPONSE_IF_NOT(details.contains("PlatformApplicabilityForPackage"),
+                                      "Missing File.ApplicabilityDetails.PlatformApplicabilityForPackage in response",
+                                      handler);
+        THROW_INVALID_RESPONSE_IF_NOT(details["PlatformApplicabilityForPackage"].is_array(),
+                                      "File.ApplicabilityDetails.PlatformApplicabilityForPackage is not an array",
+                                      handler);
+        for (const auto& app : details["PlatformApplicabilityForPackage"])
+        {
+            THROW_INVALID_RESPONSE_IF_NOT(
+                app.is_string(),
+                "File.ApplicabilityDetails.PlatformApplicabilityForPackage array value is not a string",
+                handler);
+        }
+        appEntity->applicabilityDetails.platformApplicabilityForPackage = details["PlatformApplicabilityForPackage"];
     }
 
     return tmp;
