b6f29b692149ba2c7b3447e6c3875876f4dd67a4
Arthur Araujo <arthuraraujo@microsoft.com>
2024-03-07 12:02:50 -0300

    Testing GenericFileEntitiesToFileVector

--- client/tests/unit/details/ContentUtilTests.cpp
+++ client/tests/unit/details/ContentUtilTests.cpp
@@ -11,6 +11,7 @@
 
 #define TEST(...) TEST_CASE("[ContentUtilTests] " __VA_ARGS__)
 
+using namespace SFS;
 using namespace SFS::details;
 using namespace SFS::details::contentutil;
 using namespace SFS::test;
@@ -377,6 +378,62 @@ TEST("Testing ParseJsonToFileEntity()")
                                         ServiceInvalidResponse,
                                         "File.Hashes object value is not a string");
             }
+        }
+    }
+}
+
+TEST("Testing GenericFileEntitiesToFileVector()")
+{
+    ReportingHandler handler;
+    handler.SetLoggingCallback(LogCallbackToTest);
+
+    const std::string fileId = "fileId";
+    const std::string url = "url";
+    const uint64_t size = 123;
+    const std::string sha1 = "sha1";
+    const std::string sha256 = "sha256";
+
+    SECTION("Generic File Entity")
+    {
+        std::unique_ptr<FileEntity> entity = std::make_unique<GenericFileEntity>();
+        entity->fileId = fileId;
+        entity->url = url;
+        entity->sizeInBytes = size;
+        entity->hashes = {{"Sha1", sha1}, {"Sha256", sha256}};
+
+        std::vector<std::unique_ptr<FileEntity>> entities;
+        entities.push_back(std::move(entity));
+
+        auto CheckFile = [&](const File& file) {
+            REQUIRE(file.GetFileId() == fileId);
+            REQUIRE(file.GetUrl() == url);
+            REQUIRE(file.GetSizeInBytes() == size);
+            REQUIRE(file.GetHashes().size() == 2);
+            REQUIRE(file.GetHashes().at(HashType::Sha1) == sha1);
+            REQUIRE(file.GetHashes().at(HashType::Sha256) == sha256);
+        };
+
+        std::vector<File> files;
+        SECTION("1 file")
+        {
+            REQUIRE_NOTHROW(files = GenericFileEntitiesToFileVector(std::move(entities), handler));
+            REQUIRE(files.size() == 1);
+            CheckFile(files[0]);
+        }
+
+        SECTION("2 files")
+        {
+            std::unique_ptr<FileEntity> entity2 = std::make_unique<GenericFileEntity>();
+            entity2->fileId = fileId;
+            entity2->url = url;
+            entity2->sizeInBytes = size;
+            entity2->hashes = {{"Sha1", sha1}, {"Sha256", sha256}};
+
+            entities.push_back(std::move(entity2));
+            REQUIRE_NOTHROW(files = GenericFileEntitiesToFileVector(std::move(entities), handler));
+            REQUIRE(files.size() == 2);
+            CheckFile(files[0]);
+            CheckFile(files[1]);
         }
     }
 }
