ecd6b6e0ba8cd6acf58ed412a6aeb08e6dc07d54
Arthur Araujo <153530493+arthuraraujo-msft@users.noreply.github.com>
2024-03-08 07:59:20 -0300

    Adding AppVersionEntity (#117)
    * Adding AppVersionEntity
    
    * Parsing AppVersionEntity
    
    * Adding tests for GenericVersionEntity
    
    * Adding tests for AppVersionEntity

--- client/src/details/ContentUtil.cpp
+++ client/src/details/ContentUtil.cpp
@@ -57,12 +57,32 @@ std::unique_ptr<VersionEntity> contentutil::ParseJsonToVersionEntity(const nlohm
     //   }
     // }
     //
-
-    // TODO #50: Use a different entity once the service supports app content.
-
-    std::unique_ptr<GenericVersionEntity> tmp = std::make_unique<GenericVersionEntity>();
+    // Expected extra elements for an app version entity:
+    // {
+    //   ...
+    //   "UpdateId": "<id>",
+    //   "Prerequisites": [
+    //     {
+    //       "Namespace": "<ns>",
+    //       "Name": "<name>",
+    //       "Version": "<version>"
+    //     }
+    //   ]
+    // }
 
     THROW_INVALID_RESPONSE_IF_NOT(data.is_object(), "Response is not a JSON object", handler);
+
+    std::unique_ptr<VersionEntity> tmp;
+    const bool isAppEntity = data.contains("UpdateId");
+    if (isAppEntity)
+    {
+        tmp = std::make_unique<AppVersionEntity>();
+    }
+    else
+    {
+        tmp = std::make_unique<GenericVersionEntity>();
+    }
+
     THROW_INVALID_RESPONSE_IF_NOT(data.contains("ContentId"), "Missing ContentId in response", handler);
 
     const auto& contentId = data["ContentId"];
@@ -80,6 +100,44 @@ std::unique_ptr<VersionEntity> contentutil::ParseJsonToVersionEntity(const nlohm
     THROW_INVALID_RESPONSE_IF_NOT(contentId["Version"].is_string(), "ContentId.Version is not a string", handler);
     tmp->contentId.version = contentId["Version"];
 
+    if (isAppEntity)
+    {
+        auto appEntity = dynamic_cast<AppVersionEntity*>(tmp.get());
+
+        THROW_INVALID_RESPONSE_IF_NOT(data["UpdateId"].is_string(), "UpdateId is not a string", handler);
+        appEntity->updateId = data["UpdateId"];
+
+        THROW_INVALID_RESPONSE_IF_NOT(data.contains("Prerequisites"), "Missing Prerequisites in response", handler);
+        THROW_INVALID_RESPONSE_IF_NOT(data["Prerequisites"].is_array(), "Prerequisites is not an array", handler);
+
+        for (const auto& prereq : data["Prerequisites"])
+        {
+            THROW_INVALID_RESPONSE_IF_NOT(prereq.is_object(), "Prerequisite element is not a JSON object", handler);
+
+            GenericVersionEntity prereqEntity;
+            THROW_INVALID_RESPONSE_IF_NOT(prereq.contains("Namespace"),
+                                          "Missing Prerequisite.Namespace in response",
+                                          handler);
+            THROW_INVALID_RESPONSE_IF_NOT(prereq["Namespace"].is_string(),
+                                          "Prerequisite.Namespace is not a string",
+                                          handler);
+            prereqEntity.contentId.nameSpace = prereq["Namespace"];
+
+            THROW_INVALID_RESPONSE_IF_NOT(prereq.contains("Name"), "Missing Prerequisite.Name in response", handler);
+            THROW_INVALID_RESPONSE_IF_NOT(prereq["Name"].is_string(), "Prerequisite.Name is not a string", handler);
+            prereqEntity.contentId.name = prereq["Name"];
+
+            THROW_INVALID_RESPONSE_IF_NOT(prereq.contains("Version"),
+                                          "Missing Prerequisite.Version in response",
+                                          handler);
+            THROW_INVALID_RESPONSE_IF_NOT(prereq["Version"].is_string(),
+                                          "Prerequisite.Version is not a string",
+                                          handler);
+            prereqEntity.contentId.version = prereq["Version"];
+
+            appEntity->prerequisites.push_back(std::move(prereqEntity));
+        }
+    }
     return tmp;
 }
 
