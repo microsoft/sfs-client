c95e2e2f9d8689748c95481fa1bf08ffeab76675
Arthur Araujo <arthuraraujo@microsoft.com>
2024-03-12 10:36:52 -0300

    Creating separate file for AppFile

--- client/include/sfsclient/AppContent.h
+++ client/include/sfsclient/AppContent.h
@@ -3,7 +3,7 @@
 
 #pragma once
 
-#include "ApplicabilityDetails.h"
+#include "AppFile.h"
 #include "Content.h"
 
 #include <memory>
@@ -12,50 +12,6 @@
 
 namespace SFS
 {
-class AppFile : private File
-{
-  public:
-    [[nodiscard]] static Result Make(std::string fileId,
-                                     std::string url,
-                                     uint64_t sizeInBytes,
-                                     std::unordered_map<HashType, std::string> hashes,
-                                     std::vector<Architecture> architectures,
-                                     std::vector<std::string> platformApplicabilityForPackage,
-                                     std::string fileMoniker,
-                                     std::unique_ptr<AppFile>& out) noexcept;
-
-    AppFile(AppFile&&) noexcept;
-
-    AppFile(const AppFile&) = delete;
-    AppFile& operator=(const AppFile&) = delete;
-
-    /// Getter methods from File class
-    using File::GetFileId;
-    using File::GetHashes;
-    using File::GetSizeInBytes;
-    using File::GetUrl;
-
-    /**
-     * @return Set of details related to applicability of the file
-     */
-    const ApplicabilityDetails& GetApplicabilityDetails() const noexcept;
-
-    /**
-     * @return Package Moniker of the file
-     */
-    const std::string& GetFileMoniker() const noexcept;
-
-  private:
-    AppFile(std::string&& fileId,
-            std::string&& url,
-            uint64_t sizeInBytes,
-            std::unordered_map<HashType, std::string>&& hashes,
-            std::string&& fileMoniker);
-
-    std::unique_ptr<ApplicabilityDetails> m_applicabilityDetails;
-    std::string m_fileMoniker;
-};
-
 class AppPrerequisiteContent
 {
   public:
--- client/src/AppContent.cpp
+++ client/src/AppContent.cpp
@@ -7,58 +7,6 @@
 
 using namespace SFS;
 
-AppFile::AppFile(std::string&& fileId,
-                 std::string&& url,
-                 uint64_t sizeInBytes,
-                 std::unordered_map<HashType, std::string>&& hashes,
-                 std::string&& fileMoniker)
-    : File(std::move(fileId), std::move(url), sizeInBytes, std::move(hashes))
-    , m_fileMoniker(std::move(fileMoniker))
-{
-}
-
-Result AppFile::Make(std::string fileId,
-                     std::string url,
-                     uint64_t sizeInBytes,
-                     std::unordered_map<HashType, std::string> hashes,
-                     std::vector<Architecture> architectures,
-                     std::vector<std::string> platformApplicabilityForPackage,
-                     std::string fileMoniker,
-                     std::unique_ptr<AppFile>& out) noexcept
-try
-{
-    out.reset();
-
-    std::unique_ptr<ApplicabilityDetails> details;
-    RETURN_IF_FAILED(
-        ApplicabilityDetails::Make(std::move(architectures), std::move(platformApplicabilityForPackage), details));
-
-    std::unique_ptr<AppFile> tmp(
-        new AppFile(std::move(fileId), std::move(url), sizeInBytes, std::move(hashes), std::move(fileMoniker)));
-    tmp->m_applicabilityDetails = std::move(details);
-
-    out = std::move(tmp);
-
-    return Result::Success;
-}
-SFS_CATCH_RETURN()
-
-AppFile::AppFile(AppFile&& other) noexcept : File(std::move(other))
-{
-    m_applicabilityDetails = std::move(other.m_applicabilityDetails);
-    m_fileMoniker = std::move(other.m_fileMoniker);
-}
-
-const ApplicabilityDetails& AppFile::GetApplicabilityDetails() const noexcept
-{
-    return *m_applicabilityDetails;
-}
-
-const std::string& AppFile::GetFileMoniker() const noexcept
-{
-    return m_fileMoniker;
-}
-
 Result AppPrerequisiteContent::Make(std::unique_ptr<ContentId>&& contentId,
                                     std::vector<AppFile>&& files,
                                     std::unique_ptr<AppPrerequisiteContent>& out) noexcept
