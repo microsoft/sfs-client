e34517b433f14b89ea52bed2b0c07d6229a562bc
Arthur Araujo <arthuraraujo@microsoft.com>
2024-03-13 12:21:11 -0300

    Adding more VersionEntity tests

--- client/tests/unit/details/entity/VersionEntityTests.cpp
+++ client/tests/unit/details/entity/VersionEntityTests.cpp
@@ -5,6 +5,7 @@
 #include "../../../util/TestHelper.h"
 #include "ReportingHandler.h"
 #include "entity/VersionEntity.h"
+#include "sfsclient/ContentId.h"
 
 #include <catch2/catch_test_macros.hpp>
 #include <nlohmann/json.hpp>
@@ -16,34 +17,39 @@ using namespace SFS::details;
 using namespace SFS::test;
 using json = nlohmann::json;
 
+// GenericVersionEntity constants
+const std::string c_ns = "namespace";
+const std::string c_name = "name";
+const std::string c_version = "version";
+
+// AppVersionEntity constants
+const std::string c_updateId = "updateId";
+
 TEST("Testing VersionEntity::FromJson()")
 {
     ReportingHandler handler;
     handler.SetLoggingCallback(LogCallbackToTest);
-    const std::string ns = "namespace";
-    const std::string name = "name";
-    const std::string version = "version";
 
     SECTION("Generic Version Entity")
     {
         std::unique_ptr<VersionEntity> entity;
         SECTION("Correct")
         {
-            const json versionEntity = {{"ContentId", {{"Namespace", ns}, {"Name", name}, {"Version", version}}}};
+            const json versionEntity = {{"ContentId", {{"Namespace", c_ns}, {"Name", c_name}, {"Version", c_version}}}};
 
             REQUIRE_NOTHROW(entity = VersionEntity::FromJson(versionEntity, handler));
             REQUIRE(entity != nullptr);
             REQUIRE(entity->GetContentType() == ContentType::Generic);
-            REQUIRE(entity->contentId.nameSpace == ns);
-            REQUIRE(entity->contentId.name == name);
-            REQUIRE(entity->contentId.version == version);
+            REQUIRE(entity->contentId.nameSpace == c_ns);
+            REQUIRE(entity->contentId.name == c_name);
+            REQUIRE(entity->contentId.version == c_version);
         }
 
         SECTION("Missing fields")
         {
             SECTION("Missing ContentId")
             {
-                const json versionEntity = {{"Namespace", ns}, {"Name", name}, {"Version", version}};
+                const json versionEntity = {{"Namespace", c_ns}, {"Name", c_name}, {"Version", c_version}};
                 REQUIRE_THROWS_CODE_MSG(VersionEntity::FromJson(versionEntity, handler),
                                         ServiceInvalidResponse,
                                         "Missing ContentId in response");
@@ -51,7 +57,7 @@ TEST("Testing VersionEntity::FromJson()")
 
             SECTION("Missing ContentId.Namespace")
             {
-                const json versionEntity = {{"ContentId", {{"Name", name}, {"Version", version}}}};
+                const json versionEntity = {{"ContentId", {{"Name", c_name}, {"Version", c_version}}}};
                 REQUIRE_THROWS_CODE_MSG(VersionEntity::FromJson(versionEntity, handler),
                                         ServiceInvalidResponse,
                                         "Missing ContentId.Namespace in response");
@@ -59,7 +65,7 @@ TEST("Testing VersionEntity::FromJson()")
 
             SECTION("Missing ContentId.Name")
             {
-                const json versionEntity = {{"ContentId", {{"Namespace", ns}, {"Version", version}}}};
+                const json versionEntity = {{"ContentId", {{"Namespace", c_ns}, {"Version", c_version}}}};
                 REQUIRE_THROWS_CODE_MSG(VersionEntity::FromJson(versionEntity, handler),
                                         ServiceInvalidResponse,
                                         "Missing ContentId.Name in response");
@@ -67,7 +73,7 @@ TEST("Testing VersionEntity::FromJson()")
 
             SECTION("Missing ContentId.Version")
             {
-                const json versionEntity = {{"ContentId", {{"Namespace", ns}, {"Name", name}}}};
+                const json versionEntity = {{"ContentId", {{"Namespace", c_ns}, {"Name", c_name}}}};
                 REQUIRE_THROWS_CODE_MSG(VersionEntity::FromJson(versionEntity, handler),
                                         ServiceInvalidResponse,
                                         "Missing ContentId.Version in response");
@@ -78,7 +84,7 @@ TEST("Testing VersionEntity::FromJson()")
         {
             SECTION("ContentId not an object")
             {
-                const json versionEntity = json::array({{"Namespace", 1}, {"Name", name}, {"Version", version}});
+                const json versionEntity = json::array({{"Namespace", 1}, {"Name", c_name}, {"Version", c_version}});
                 REQUIRE_THROWS_CODE_MSG(VersionEntity::FromJson(versionEntity, handler),
                                         ServiceInvalidResponse,
                                         "Response is not a JSON object");
@@ -86,7 +92,8 @@ TEST("Testing VersionEntity::FromJson()")
 
             SECTION("ContentId.Namespace")
             {
-                const json versionEntity = {{"ContentId", {{"Namespace", 1}, {"Name", name}, {"Version", version}}}};
+                const json versionEntity = {
+                    {"ContentId", {{"Namespace", 1}, {"Name", c_name}, {"Version", c_version}}}};
                 REQUIRE_THROWS_CODE_MSG(VersionEntity::FromJson(versionEntity, handler),
                                         ServiceInvalidResponse,
                                         "ContentId.Namespace is not a string");
@@ -94,7 +101,7 @@ TEST("Testing VersionEntity::FromJson()")
 
             SECTION("ContentId.Name")
             {
-                const json versionEntity = {{"ContentId", {{"Namespace", ns}, {"Name", 1}, {"Version", version}}}};
+                const json versionEntity = {{"ContentId", {{"Namespace", c_ns}, {"Name", 1}, {"Version", c_version}}}};
                 REQUIRE_THROWS_CODE_MSG(VersionEntity::FromJson(versionEntity, handler),
                                         ServiceInvalidResponse,
                                         "ContentId.Name is not a string");
@@ -102,7 +109,7 @@ TEST("Testing VersionEntity::FromJson()")
 
             SECTION("ContentId.Version")
             {
-                const json versionEntity = {{"ContentId", {{"Namespace", ns}, {"Name", name}, {"Version", 1}}}};
+                const json versionEntity = {{"ContentId", {{"Namespace", c_ns}, {"Name", c_name}, {"Version", 1}}}};
                 REQUIRE_THROWS_CODE_MSG(VersionEntity::FromJson(versionEntity, handler),
                                         ServiceInvalidResponse,
                                         "ContentId.Version is not a string");
@@ -112,36 +119,35 @@ TEST("Testing VersionEntity::FromJson()")
 
     SECTION("App Version Entity")
     {
-        const std::string updateId = "updateId";
         std::unique_ptr<VersionEntity> entity;
-        const json contentId = {{"Namespace", ns}, {"Name", name}, {"Version", version}};
+        const json contentId = {{"Namespace", c_ns}, {"Name", c_name}, {"Version", c_version}};
 
         SECTION("Correct")
         {
             const json versionEntity = {{"ContentId", contentId},
-                                        {"UpdateId", updateId},
+                                        {"UpdateId", c_updateId},
                                         {"Prerequisites", json::array({contentId})}};
 
             REQUIRE_NOTHROW(entity = VersionEntity::FromJson(versionEntity, handler));
             REQUIRE(entity != nullptr);
             REQUIRE(entity->GetContentType() == ContentType::App);
-            REQUIRE(entity->contentId.nameSpace == ns);
-            REQUIRE(entity->contentId.name == name);
-            REQUIRE(entity->contentId.version == version);
+            REQUIRE(entity->contentId.nameSpace == c_ns);
+            REQUIRE(entity->contentId.name == c_name);
+            REQUIRE(entity->contentId.version == c_version);
 
             AppVersionEntity* appEntity = dynamic_cast<AppVersionEntity*>(entity.get());
-            REQUIRE(appEntity->updateId == updateId);
+            REQUIRE(appEntity->updateId == c_updateId);
             REQUIRE(appEntity->prerequisites.size() == 1);
-            REQUIRE(appEntity->prerequisites[0].contentId.nameSpace == ns);
-            REQUIRE(appEntity->prerequisites[0].contentId.name == name);
-            REQUIRE(appEntity->prerequisites[0].contentId.version == version);
+            REQUIRE(appEntity->prerequisites[0].contentId.nameSpace == c_ns);
+            REQUIRE(appEntity->prerequisites[0].contentId.name == c_name);
+            REQUIRE(appEntity->prerequisites[0].contentId.version == c_version);
         }
 
         SECTION("Missing fields")
         {
             SECTION("Missing Prerequisites")
             {
-                const json versionEntity = {{"ContentId", contentId}, {"UpdateId", updateId}};
+                const json versionEntity = {{"ContentId", contentId}, {"UpdateId", c_updateId}};
                 REQUIRE_THROWS_CODE_MSG(VersionEntity::FromJson(versionEntity, handler),
                                         ServiceInvalidResponse,
                                         "Missing Prerequisites in response");
@@ -149,9 +155,9 @@ TEST("Testing VersionEntity::FromJson()")
 
             SECTION("Missing Prerequisite.Namespace")
             {
-                const json wrongPrerequisite = {{"Name", name}, {"Version", version}};
+                const json wrongPrerequisite = {{"Name", c_name}, {"Version", c_version}};
                 const json versionEntity = {{"ContentId", contentId},
-                                            {"UpdateId", updateId},
+                                            {"UpdateId", c_updateId},
                                             {"Prerequisites", json::array({wrongPrerequisite})}};
                 REQUIRE_THROWS_CODE_MSG(VersionEntity::FromJson(versionEntity, handler),
                                         ServiceInvalidResponse,
@@ -160,9 +166,9 @@ TEST("Testing VersionEntity::FromJson()")
 
             SECTION("Missing Prerequisite.Name")
             {
-                const json wrongPrerequisite = {{"Namespace", ns}, {"Version", version}};
+                const json wrongPrerequisite = {{"Namespace", c_ns}, {"Version", c_version}};
                 const json versionEntity = {{"ContentId", contentId},
-                                            {"UpdateId", updateId},
+                                            {"UpdateId", c_updateId},
                                             {"Prerequisites", json::array({wrongPrerequisite})}};
                 REQUIRE_THROWS_CODE_MSG(VersionEntity::FromJson(versionEntity, handler),
                                         ServiceInvalidResponse,
@@ -171,9 +177,9 @@ TEST("Testing VersionEntity::FromJson()")
 
             SECTION("Missing Prerequisite.Version")
             {
-                const json wrongPrerequisite = {{"Namespace", ns}, {"Name", name}};
+                const json wrongPrerequisite = {{"Namespace", c_ns}, {"Name", c_name}};
                 const json versionEntity = {{"ContentId", contentId},
-                                            {"UpdateId", updateId},
+                                            {"UpdateId", c_updateId},
                                             {"Prerequisites", json::array({wrongPrerequisite})}};
                 REQUIRE_THROWS_CODE_MSG(VersionEntity::FromJson(versionEntity, handler),
                                         ServiceInvalidResponse,
@@ -198,7 +204,7 @@ TEST("Testing VersionEntity::FromJson()")
                 SECTION("Not an array")
                 {
                     const json versionEntity = {{"ContentId", contentId},
-                                                {"UpdateId", updateId},
+                                                {"UpdateId", c_updateId},
                                                 {"Prerequisites", contentId}};
                     REQUIRE_THROWS_CODE_MSG(VersionEntity::FromJson(versionEntity, handler),
                                             ServiceInvalidResponse,
@@ -208,7 +214,7 @@ TEST("Testing VersionEntity::FromJson()")
                 SECTION("Element is not an object")
                 {
                     const json versionEntity = {{"ContentId", contentId},
-                                                {"UpdateId", updateId},
+                                                {"UpdateId", c_updateId},
                                                 {"Prerequisites", json::array({1})}};
                     REQUIRE_THROWS_CODE_MSG(VersionEntity::FromJson(versionEntity, handler),
                                             ServiceInvalidResponse,
@@ -217,9 +223,9 @@ TEST("Testing VersionEntity::FromJson()")
 
                 SECTION("Prerequisite.Namespace")
                 {
-                    const json wrongPrerequisite = {{"Namespace", 1}, {"Name", name}, {"Version", version}};
+                    const json wrongPrerequisite = {{"Namespace", 1}, {"Name", c_name}, {"Version", c_version}};
                     const json versionEntity = {{"ContentId", contentId},
-                                                {"UpdateId", updateId},
+                                                {"UpdateId", c_updateId},
                                                 {"Prerequisites", json::array({wrongPrerequisite})}};
                     REQUIRE_THROWS_CODE_MSG(VersionEntity::FromJson(versionEntity, handler),
                                             ServiceInvalidResponse,
@@ -228,9 +234,9 @@ TEST("Testing VersionEntity::FromJson()")
 
                 SECTION("Prerequisite.Name")
                 {
-                    const json wrongPrerequisite = {{"Namespace", ns}, {"Name", 1}, {"Version", version}};
+                    const json wrongPrerequisite = {{"Namespace", c_ns}, {"Name", 1}, {"Version", c_version}};
                     const json versionEntity = {{"ContentId", contentId},
-                                                {"UpdateId", updateId},
+                                                {"UpdateId", c_updateId},
                                                 {"Prerequisites", json::array({wrongPrerequisite})}};
                     REQUIRE_THROWS_CODE_MSG(VersionEntity::FromJson(versionEntity, handler),
                                             ServiceInvalidResponse,
@@ -239,15 +245,61 @@ TEST("Testing VersionEntity::FromJson()")
 
                 SECTION("Prerequisite.Version")
                 {
-                    const json wrongPrerequisite = {{"Namespace", ns}, {"Name", name}, {"Version", 1}};
+                    const json wrongPrerequisite = {{"Namespace", c_ns}, {"Name", c_name}, {"Version", 1}};
                     const json versionEntity = {{"ContentId", contentId},
-                                                {"UpdateId", updateId},
+                                                {"UpdateId", c_updateId},
                                                 {"Prerequisites", json::array({wrongPrerequisite})}};
                     REQUIRE_THROWS_CODE_MSG(VersionEntity::FromJson(versionEntity, handler),
                                             ServiceInvalidResponse,
                                             "Prerequisite.Version is not a string");
                 }
             }
+        }
+    }
+}
+
+TEST("Testing VersionEntity conversions")
+{
+    ReportingHandler handler;
+    handler.SetLoggingCallback(LogCallbackToTest);
+
+    auto CheckContentId = [&](const ContentId& contentId) {
+        REQUIRE(contentId.GetNameSpace() == c_ns);
+        REQUIRE(contentId.GetName() == c_name);
+        REQUIRE(contentId.GetVersion() == c_version);
+    };
+
+    SECTION("VersionEntity::ToContentId()")
+    {
+        SECTION("Success with GenericVersionEntity")
+        {
+            std::unique_ptr<VersionEntity> entity = std::make_unique<GenericVersionEntity>();
+            entity->contentId.nameSpace = c_ns;
+            entity->contentId.name = c_name;
+            entity->contentId.version = c_version;
+
+            auto contentId = VersionEntity::ToContentId(std::move(*entity), handler);
+            CheckContentId(*contentId);
+        }
+
+        SECTION("Success with AppVersionEntity")
+        {
+            std::unique_ptr<VersionEntity> entity = std::make_unique<AppVersionEntity>();
+            auto appEntity = AppVersionEntity::GetAppVersionEntityPtr(entity, handler);
+            appEntity->contentId.nameSpace = c_ns;
+            appEntity->contentId.name = c_name;
+            appEntity->contentId.version = c_version;
+            appEntity->updateId = c_updateId;
+
+            std::unique_ptr<GenericVersionEntity> prereqEntity = std::make_unique<GenericVersionEntity>();
+            prereqEntity->contentId.nameSpace = c_ns;
+            prereqEntity->contentId.name = c_name;
+            prereqEntity->contentId.version = c_version;
+
+            appEntity->prerequisites.push_back(std::move(*prereqEntity));
+
+            auto contentId = VersionEntity::ToContentId(std::move(*entity), handler);
+            CheckContentId(*contentId);
         }
     }
 }
