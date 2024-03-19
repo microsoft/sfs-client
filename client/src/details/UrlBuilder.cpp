287be437b99c3005ccc464e0b9ab490c0d8cd88c
Arthur Araujo <153530493+arthuraraujo-msft@users.noreply.github.com>
2024-03-19 16:32:56 -0300

    Adding UrlBuilder (#168)
    * Adding UrlBuilder
    
    * Adding tests
    
    * Using std::unique_ptr
    
    * Throw if constructor fails
    
    * Adding memory header
    
    * Renaming macro
    
    * Adding AppendQuery()
    
    * Adding constructor with url param

---
+++ client/src/details/UrlBuilder.cpp
@@ -0,0 +1,107 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+#include "UrlBuilder.h"
+
+#include "ErrorHandling.h"
+#include "ReportingHandler.h"
+
+#include <curl/curl.h>
+
+#include <memory>
+
+#define THROW_IF_CURL_URL_ERROR(curlCall, error)                                                                       \
+    do                                                                                                                 \
+    {                                                                                                                  \
+        auto __curlUrlCode = (curlCall);                                                                               \
+        THROW_CODE_IF_NOT_LOG(error, __curlUrlCode == CURLUE_OK, m_handler, GetCurlUrlStrError(__curlUrlCode));        \
+    } while ((void)0, 0)
+
+#define THROW_IF_CURL_URL_SETUP_ERROR(curlCall) THROW_IF_CURL_URL_ERROR(curlCall, ConnectionUrlSetupFailed)
+
+using namespace SFS::details;
+
+namespace
+{
+struct CurlCharDeleter
+{
+    void operator()(char* val)
+    {
+        if (val)
+        {
+            curl_free(val);
+        }
+    }
+};
+
+using CurlCharPtr = std::unique_ptr<char, CurlCharDeleter>;
+
+std::string GetCurlUrlStrError(CURLUcode code)
+{
+    return "Curl URL error: " + std::string(curl_url_strerror(code));
+}
+} // namespace
+
+UrlBuilder::UrlBuilder(const ReportingHandler& handler) : m_handler(handler)
+{
+    m_handle = curl_url();
+    THROW_CODE_IF_NOT_LOG(ConnectionUrlSetupFailed, m_handle, m_handler, "Curl URL error: Failed to create URL");
+}
+
+UrlBuilder::UrlBuilder(const std::string& url, const ReportingHandler& handler) : UrlBuilder(handler)
+{
+    SetUrl(url);
+}
+
+UrlBuilder::~UrlBuilder()
+{
+    curl_url_cleanup(m_handle);
+}
+
+std::string UrlBuilder::GetUrl() const
+{
+    CurlCharPtr url;
+    char* urlPtr = url.get();
+    THROW_IF_CURL_URL_SETUP_ERROR(curl_url_get(m_handle, CURLUPART_URL, &urlPtr, 0 /*flags*/));
+    return urlPtr;
+}
+
+void UrlBuilder::SetScheme(Scheme scheme)
+{
+    switch (scheme)
+    {
+    case Scheme::Https:
+        THROW_IF_CURL_URL_SETUP_ERROR(curl_url_set(m_handle, CURLUPART_SCHEME, "https", 0 /*flags*/));
+        break;
+    }
+}
+
+void UrlBuilder::SetHost(const std::string& host)
+{
+    THROW_IF_CURL_URL_SETUP_ERROR(curl_url_set(m_handle, CURLUPART_HOST, host.c_str(), 0 /*flags*/));
+}
+
+void UrlBuilder::SetPath(const std::string& path, bool encode)
+{
+    unsigned flags = 0;
+    if (encode)
+    {
+        flags |= CURLU_URLENCODE;
+    }
+    THROW_IF_CURL_URL_SETUP_ERROR(curl_url_set(m_handle, CURLUPART_PATH, path.c_str(), flags));
+}
+
+void UrlBuilder::SetQuery(const std::string& query)
+{
+    THROW_IF_CURL_URL_SETUP_ERROR(curl_url_set(m_handle, CURLUPART_QUERY, query.c_str(), 0 /*flags*/));
+}
+
+void UrlBuilder::AppendQuery(const std::string& query)
+{
+    THROW_IF_CURL_URL_SETUP_ERROR(curl_url_set(m_handle, CURLUPART_QUERY, query.c_str(), CURLU_APPENDQUERY));
+}
+
+void UrlBuilder::SetUrl(const std::string& url)
+{
+    THROW_IF_CURL_URL_SETUP_ERROR(curl_url_set(m_handle, CURLUPART_URL, url.c_str(), 0 /*flags*/));
+}
---
+++ client/tests/unit/details/UrlBuilderTests.cpp
@@ -0,0 +1,107 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+#include "../../util/SFSExceptionMatcher.h"
+#include "../../util/TestHelper.h"
+#include "ReportingHandler.h"
+#include "UrlBuilder.h"
+
+#include <catch2/catch_test_macros.hpp>
+
+#define TEST(...) TEST_CASE("[UrlBuilderTests] " __VA_ARGS__)
+
+using namespace SFS::details;
+using namespace SFS::test;
+
+TEST("UrlBuilder")
+{
+    ReportingHandler handler;
+    handler.SetLoggingCallback(LogCallbackToTest);
+
+    UrlBuilder builder(handler);
+    REQUIRE_THROWS_CODE_MSG(builder.GetUrl(), ConnectionUrlSetupFailed, "Curl URL error: No host part in the URL");
+
+    builder.SetHost("www.example.com");
+    REQUIRE_THROWS_CODE_MSG(builder.GetUrl(), ConnectionUrlSetupFailed, "Curl URL error: No scheme part in the URL");
+
+    builder.SetScheme(Scheme::Https);
+    REQUIRE(builder.GetUrl() == "https://www.example.com/");
+
+    SECTION("SetHost")
+    {
+        builder.SetHost("www.example2.com");
+        REQUIRE(builder.GetUrl() == "https://www.example2.com/");
+
+        REQUIRE_THROWS_CODE_MSG(builder.SetHost("+"), ConnectionUrlSetupFailed, "Curl URL error: Bad hostname");
+        REQUIRE_THROWS_CODE_MSG(builder.SetHost("a&b"), ConnectionUrlSetupFailed, "Curl URL error: Bad hostname");
+        REQUIRE_THROWS_CODE_MSG(builder.SetHost("a\nb"), ConnectionUrlSetupFailed, "Curl URL error: Bad hostname");
+        REQUIRE_THROWS_CODE_MSG(builder.SetHost("a\tb"), ConnectionUrlSetupFailed, "Curl URL error: Bad hostname");
+    }
+
+    SECTION("SetPath")
+    {
+        builder.SetPath("index.html");
+        REQUIRE(builder.GetUrl() == "https://www.example.com/index.html");
+
+        builder.SetPath("index.html", true);
+        REQUIRE(builder.GetUrl() == "https://www.example.com/index.html");
+
+        builder.SetPath("index>@", true);
+        REQUIRE(builder.GetUrl() == "https://www.example.com/index%3e%40");
+
+        INFO("Encoding skips the / character");
+        builder.SetPath("index>@/index", true);
+        REQUIRE(builder.GetUrl() == "https://www.example.com/index%3e%40/index");
+    }
+
+    SECTION("SetQuery, AppendQuery")
+    {
+        builder.SetQuery("key=value");
+        REQUIRE(builder.GetUrl() == "https://www.example.com/?key=value");
+
+        builder.AppendQuery("key2=value2");
+        REQUIRE(builder.GetUrl() == "https://www.example.com/?key=value&key2=value2");
+
+        builder.SetQuery("key2=value2");
+        REQUIRE(builder.GetUrl() == "https://www.example.com/?key2=value2");
+    }
+
+    SECTION("SetUrl")
+    {
+        builder.SetUrl("https://www.example.com/index.html?key=value");
+        REQUIRE(builder.GetUrl() == "https://www.example.com/index.html?key=value");
+        REQUIRE_THROWS_CODE_MSG(builder.SetUrl("https://www.+.com"),
+                                ConnectionUrlSetupFailed,
+                                "Curl URL error: Bad hostname");
+    }
+
+    SECTION("SetScheme, SetHost, SetPath, SetQuery")
+    {
+        builder.SetScheme(Scheme::Https);
+        builder.SetHost("www.example.com");
+        builder.SetPath("index.html");
+        builder.SetQuery("key=value");
+
+        REQUIRE(builder.GetUrl() == "https://www.example.com/index.html?key=value");
+    }
+
+    SECTION("SetScheme, SetHost, SetPath, SetQuery with encoding")
+    {
+        builder.SetScheme(Scheme::Https);
+        builder.SetHost("www.example.com");
+        builder.SetPath("index@.html", true);
+        builder.SetQuery("key=value");
+
+        REQUIRE(builder.GetUrl() == "https://www.example.com/index%40.html?key=value");
+    }
+
+    SECTION("Constructor with URL")
+    {
+        UrlBuilder builder2("https://www.example.com/index.html?key=value", handler);
+        REQUIRE(builder2.GetUrl() == "https://www.example.com/index.html?key=value");
+
+        REQUIRE_THROWS_CODE_MSG(UrlBuilder("https://www.+.com", handler),
+                                ConnectionUrlSetupFailed,
+                                "Curl URL error: Bad hostname");
+    }
+}
