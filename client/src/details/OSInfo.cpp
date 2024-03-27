38df4765ef231f6cd2f4fb57acef26e6d43b2111
Arthur Araujo <arthuraraujo@microsoft.com>
2024-03-27 10:16:10 -0300

    Simplifying OS versions

--- client/src/details/OSInfo.cpp
+++ client/src/details/OSInfo.cpp
@@ -7,18 +7,13 @@
 
 #include <windows.h>
 
-#include <VersionHelpers.h>
 #include <sysinfoapi.h>
 
 #elif __GNUG__
 
-#include <filesystem>
-#include <fstream>
 #include <sys/utsname.h>
 
 #endif
-
-#include <sstream>
 
 using namespace SFS::details;
 
@@ -27,10 +22,7 @@ namespace
 #ifdef _WIN32
 std::string GetWindowsOSVersion()
 {
-    // In Windows, the API below only works if the application has a manifest file with indications to the supported OS
-    // version (https://learn.microsoft.com/en-us/windows/win32/sysinfo/targeting-your-application-at-windows-8-1).
-    // If the caller application does not have a manifest file, this function will just return "Windows"
-    return IsWindows10OrGreater() ? "Windows NT 10.0" : "Windows";
+    return "Windows";
 }
 
 std::string GetWindowsMachineInfo()
@@ -57,24 +49,7 @@ std::string GetWindowsMachineInfo()
 #ifdef __GNUG__
 std::string GetLinuxOSVersion()
 {
-    // Check for /etc/os-release file, which contains information about the distribution
-    static const std::string s_osReleaseFile = "/etc/os-release";
-    if (std::filesystem::exists(s_osReleaseFile))
-    {
-        std::ifstream file(s_osReleaseFile);
-        std::string line;
-        while (std::getline(file, line))
-        {
-            // The PRETTY_NAME line contains the name of the distribution like PRETTY_NAME="Ubuntu 22.04.3 LTS"
-            if (line.find("PRETTY_NAME") != std::string::npos)
-            {
-                // Extract the name of the distribution by removing the quotes
-                return line.substr(line.find("\"") + 1, line.rfind("\"") - line.find("\"") - 1);
-            }
-        }
-    }
-
-    // Linux distro not found, return kernel name
+    // Return kernel name, usually "Linux"
     utsname buf;
     if (uname(&buf) >= 0)
     {
